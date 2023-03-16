63f6e6fa8fe8d6ae0d5b21e4daa61dbe8d109ca0
Nikhlesh Rao <38323670+Nikhlesh117@users.noreply.github.com>
2023-03-16 11:01:46 +0530

    Added: Alter Table

---
+++ SQLQuery.sql
@@ -0,0 +1,34 @@
+-- Welcome to the Employee Payroll - Database
+
+--UC01: Create Database
+create database payroll_service 
+
+--UC02: Create Table
+create Table EmployeePayroll
+(
+id int identity primary key,
+name varchar(200) not null,
+salary money not null,
+start_Date date not null
+)
+
+--UC03: Insert values in Table
+Insert into EmployeePayroll values
+('Nick',20000,'2023-01-12'),
+('Suresh',25000,'2020-02-18'),
+('Sam',10000,'2022-11-13'),
+('Gitesh',30000,'2019-06-09')
+
+--UC04: Retrieve All data
+select * from EmployeePayroll
+
+-- UC05: Retrieve salary for particular employee 
+select salary from EmployeePayroll where name='Nick'
+-- Retrieve salary for particular employee who join in particular date using getdate()
+select salary from  EmployeePayroll where start_Date between '2020-01-12' and getdate()
+
+--UC06: Alter Table
+alter table EmployeePayroll add gender char(1)
+-- update row
+update EmployeePayroll set gender='F' where name='Sam' or name='Gitesh'
+update EmployeePayroll set gender ='M' where id in (1,2)
